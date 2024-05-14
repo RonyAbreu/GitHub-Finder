f00b90f13ac5aa3072458b7105c4daedb3cabbd2
RonyAbreu <ronyelison78@gmail.com>
2024-05-14 11:04:39 -0300

    Create: criando tipos de usuário e repositório

---
+++ src/types/repository.ts
@@ -0,0 +1,8 @@
+export interface RepositoryProps {
+    name: string;
+    html_url: string;
+    language: string;
+    clone_url : string;
+    stargazers_count: number;
+    forks_count: number;
+}
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+export interface UserProps {
+  avatar_url: string;
+  login: string;
+  location: string;
+  bio: string;
+  followers: number;
+  following: number;
+}
