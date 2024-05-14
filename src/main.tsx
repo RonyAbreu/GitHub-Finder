d0321655ca6faa9bc4439ce5f87c09890d1475c8
RonyAbreu <ronyelison78@gmail.com>
2024-05-14 11:02:13 -0300

    First: criação do projeto

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home.tsx";
+import PageNotFound from "./routes/PageNotFound.tsx";
+import Repository from "./routes/Repository.tsx";
+
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index path="/" element={<Home />}/>
+          <Route index path="/repos/:username" element={<Repository />}/>
+          <Route path="*" element={<PageNotFound />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
